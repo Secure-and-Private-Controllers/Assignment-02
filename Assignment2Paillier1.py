16694e956e11d38035c105e313e6e961e8a032ee
TCIdsardi <tjallingidsardi@gmail.com>
2022-06-02 11:53:38 +0200

    Update Assignment2Paillier1.py

--- Assignment2Paillier1.py
+++ Assignment2Paillier1.py
@@ -23,14 +23,15 @@ q = [1, 1, 1]
 rho = 1
 
 xg = np.zeros(iter_max+1)
+xg[0] = 1/n * np.sum(x[0])
 u = np.zeros((iter_max+1, n))
 
-for k in range(iter_max+1):
+for k in range(iter_max):
     
     for i in range(3):
-        x[k+1, i] = np.argmin(x[i].transpose()*q[i]*x[i] + rho/2 * (np.absolute(x[i] - xg[k] + u[k, i]))^2) #Not sure how to do this
+        x[k+1, i] = np.argmin(x[k, i].transpose()*q[i]*x[k, i] + rho/2 * (np.absolute(x[k, i] - xg[k] + u[k, i]))**2) #Not sure how to do this
         
-    xg = 1/n * np.sum(x, axis=1)
+    xg [k+1] = 1/n * np.sum(x[k+1])
     
     for i in range(3):
         u[k+1, i] = u[k, i] + x[k+1, i] - xg[k+1]
